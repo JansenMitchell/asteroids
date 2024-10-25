555358d3b2058d39cfedc0240f3a625613fbebac
JansenMitchell <jansenmitchellp@gmail.com>
2024-10-25 15:30:55 -0500

    Add frame rate control in main function

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         screen.fill((0, 0, 0))
@@ -15,6 +18,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        dt = fps_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
