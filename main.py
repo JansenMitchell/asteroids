aaf5b3695c42b5547ba24861e8505b48f1dffacd
JansenMitchell <jansenmitchellp@gmail.com>
2024-10-31 11:25:25 -0500

    Refactor main function to use sprite groups for updating and drawing game objects

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     fps_clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     
     while True:
@@ -21,10 +24,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
-        
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for obj in updatable:
+            obj.update(dt)
+            
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
 
 if __name__ == "__main__":
